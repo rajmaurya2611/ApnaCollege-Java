7cc4d013677cb1942e5104f7f73a30afd9a3a342
Raj Maurya <89681751+rajmaurya2611@users.noreply.github.com>
2023-03-10 04:13:26 +0530

    Add files via upload
    Calculator that takes operands and operation to be performed (in symbol form) and produce ouutput

---
+++ Calculator.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Calculator{
+    public static void main(String args[]){
+        int a,b,res;
+        char op;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Your Numbers");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println("Enter Operation to be performed");
+        op=sc.next().charAt(0);
+        switch(op){
+        case '+':
+        res=a+b;
+        System.out.println(res);
+        break;
+        case '-':
+        res=a-b;
+        System.out.println(res);
+        break;
+        case '*':
+        res=a*b;
+        System.out.println(res);
+        break;
+        case '/':
+        res=a/b;
+        System.out.println(res);
+        break;
+        case '%':
+        res=a%b;
+        System.out.println(res);
+        break;
+    }
+}
+}
